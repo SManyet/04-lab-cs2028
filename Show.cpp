22fffb52334050fa40f0dbd12ca44587224f7dff
lukelc23 <60450741+lukelc23@users.noreply.github.com>
2020-02-12 15:00:39 -0500

    1st Task

---
+++ Show.cpp
@@ -0,0 +1,31 @@
+#include "Show.h"
+#include <iostream>
+
+Show::Show(){
+    title = " ";
+    description = " ";
+}
+
+Show::Show(string makeTitle, string makeDescription){
+    title = makeTitle;
+    description = makeDescription;
+}
+
+//functions
+virtual void Show::play(){
+
+}
+void Show::details(){
+    std::cout << "Title: " << title
+    std::cout << "Description: " << description
+}
+
+
+
+//Task 1:
+//Goal: create base class (abstractly models an online streaming service)
+//  a- 2 attributes needed (Title, Description)
+//  b- default constructor and an overloaded constructor that allows setting values for all attributes
+//  c- Define a virtual function play.  Define a non-virtual function called Details
+//     that prints out hte two attributes from 1a
+//  d- Include in submission how each member will be available in derived classes
---
+++ Show.h
@@ -0,0 +1,30 @@
+#ifndef LAB4_SHOW_H
+#define LAB4_SHOW_H
+
+
+//Task 1:
+//Goal: create base class (abstractly models an online streaming service)
+//  a- 2 attributes needed (Title, Description)
+//  b- default constructor and an overloaded constructor that allows setting values for all attributes
+//  c- Define a virtual function play.  Define a non-virtual function called Details
+//     that prints out hte two attributes from 1a
+//  d- Include in submission how each member will be available in derived classes
+
+class Show {
+private:
+    //two attributes for abstract class
+    string title;
+    string description;
+
+public:
+    //two constructors
+    Show();
+    Show(string title, string description);
+
+    //functions
+    virtual void play();
+    void details();
+};
+
+
+#endif //LAB4_SHOW_H
