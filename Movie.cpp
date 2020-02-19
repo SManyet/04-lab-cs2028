b13b9cada478cb0221671ecf9ad9fc9356113dcd
SManyet <smanyetbusiness@gmail.com>
2020-02-18 21:03:22 -0500

    final clean for submission

--- Movie.cpp
+++ Movie.cpp
@@ -18,7 +18,7 @@ string Movie::getOpeningCredits(){
 Movie::Movie(string makeTitle, string makeDescription) : Show (makeTitle, makeDescription) {
     title = makeTitle;
     description = makeDescription;
-    openingCredits = "These are the opening credits.";
+    openingCredits = "These are the opening credits.\n";
 };
 
 Movie::Movie() : Show() {
